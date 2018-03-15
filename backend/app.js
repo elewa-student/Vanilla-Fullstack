7311c72993058bf6d39e21fae8c094eea2476eeb
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-03-15 13:38:26 +0100

    cors module again

--- backend/app.js
+++ backend/app.js
@@ -15,14 +15,11 @@ var bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+var cors = require('cors');
 
 // cross origin resource sharing
 // for accepting traffic from gh-pages while hosted on heroku
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 // routes =>
 
